a25c8ca31aa7b9b2822f509d2e1312ab9e9ef4bd
Mekin-jema <mekinjemal999@gmail.com>
2026-01-31 23:05:29 +0300

    Remove unused Document import and simplify transform function in applyBaseSchemaOptions

--- src/Models/base.ts
+++ src/Models/base.ts
@@ -1,10 +1,10 @@
-import type { Schema, Document } from 'mongoose';
+import type { Schema } from 'mongoose';
 
-
+export function applyBaseSchemaOptions(schema: Schema) {
   schema.set('toJSON', {
     virtuals: true,
     versionKey: false,
-    transform: (_doc: Document, ret: Record<string, any>) => {
+    transform: (_doc, ret) => {
       if (ret._id) {
         ret.id = ret._id.toString();
         delete ret._id;
